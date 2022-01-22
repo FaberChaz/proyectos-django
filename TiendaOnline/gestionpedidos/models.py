48bf10e8d4b36f5a92467c837052b42e62fc0838
Faber <denver0205@gmail.com>
2022-01-21 23:29:45 -0300

    crado un super usuario en django

--- TiendaOnline/gestionpedidos/models.py
+++ TiendaOnline/gestionpedidos/models.py
@@ -8,10 +8,16 @@ class Clientes(models.Model):
     email = models.EmailField()
     telefono = models.CharField(max_length=11)
 
+    def __str__(self):
+        return 'Nombre: %s, Apellido: %s, Direccion: %s, Email: %s, Telefono: %s' %(self.nombre, self.apellidos, self.direccion, self.email, self.telefono)
+
 class Articulo(models.Model):
     nombre = models.CharField(max_length=30)
     seccion = models.CharField(max_length=50)
     precio = models.IntegerField()
+
+    def __str__(self):
+        return 'Nombre producto: %s, Seccion: %s, Precio: %s' %(self.nombre, self.seccion, self.precio)
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
